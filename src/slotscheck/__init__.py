8ae77e66303094d9a8809a79db3e9c8cdaa9aa1e
Arie Bovenberg <a.c.bovenberg@gmail.com>
2022-01-03 12:42:12 +0100

    cleanup, fix py37 error

--- src/slotscheck/__init__.py
+++ src/slotscheck/__init__.py
@@ -5,22 +5,10 @@ import logging
 import pkgutil
 import sys
 from collections import defaultdict
-from dataclasses import dataclass
 from types import ModuleType
-from typing import (
-    Callable,
-    Collection,
-    FrozenSet,
-    Iterable,
-    Iterator,
-    Mapping,
-    TypeVar,
-)
+from typing import Callable, Collection, Iterable, Iterator, Mapping, TypeVar
 
 import click
-
-T1 = TypeVar("T1")
-T2 = TypeVar("T2")
 
 logger = logging.getLogger(__name__)
 
@@ -30,92 +18,6 @@ try:
     __version__ = __import__("importlib.metadata").metadata.version(__name__)
 except ModuleNotFoundError:  # pragma: no cover
     __version__ = __import__("importlib_metadata").version(__name__)
-
-
-@enum.unique
-class SlotsStatus(enum.Enum):
-    NO_SLOTS = enum.auto()
-    HAS_SLOTS = enum.auto()
-    NOT_PUREPYTHON = enum.auto()
-
-
-def _all_bases_have_slots(c: type) -> bool:
-    return all(slot_status(b) is not SlotsStatus.NO_SLOTS for b in c.__bases__)
-
-
-def walk_classes(module: ModuleType) -> Iterator[type]:
-    yield from (
-        klass
-        for _, klass in inspect.getmembers(module, inspect.isclass)
-        if klass.__module__.startswith(module.__name__)
-    )
-    try:
-        module_path = module.__path__[0]  # type: ignore
-    except (AttributeError, IndexError):
-        return  # it's not a package with submodules
-
-    packages = pkgutil.walk_packages([module_path])
-    for finder, name, _ in packages:
-        if not getattr(finder, "path", "").startswith(module_path):
-            continue
-
-        module_name = f"{module.__name__}.{name}"
-        try:
-            next_module = importlib.import_module(module_name)
-        except BaseException as e:
-            # sometimes packages make it impossible to import
-            # certain modules directly or out of order, or without
-            # some system or dependency requirement.
-            logger.warning("Couldn't import %s. (%r)", module_name, e)
-        else:
-            yield from walk_classes(next_module)
-
-
-def slot_status(c: type) -> SlotsStatus:
-    if "__slots__" in c.__dict__:
-        return SlotsStatus.HAS_SLOTS
-    elif _is_purepython_class(c):
-        return SlotsStatus.NO_SLOTS
-    else:
-        return SlotsStatus.NOT_PUREPYTHON
-
-
-def _class_fullname(k: type) -> str:
-    return f"{k.__module__}.{k.__qualname__}"
-
-
-def _is_purepython_class(t: type) -> bool:
-    "whether a class is defined in Python, or an extension/C module"
-    # There is no easy way to check if a class is pure Python.
-    # One symptom of a non-native class is that it is not possible to
-    # set attributes on it. Let's do that.
-    try:
-        t._SLOTSCHECK_POKE = 1  # type: ignore
-    except TypeError as e:
-        if e.args[0].startswith(
-            "cannot set '_SLOTSCHECK_POKE' attribute of immutable type"
-        ):
-            return False
-        raise  # some other error we may want to know about
-    else:
-        del t._SLOTSCHECK_POKE  # type: ignore
-        return True
-
-
-def _groupby(
-    it: Iterable[T1], key: Callable[[T1], T2]
-) -> Mapping[T2, Collection[T1]]:
-    grouped = defaultdict(list)
-    for i in it:
-        grouped[key(i)].append(i)
-
-    return grouped
-
-
-@dataclass(frozen=True)
-class BrokenSlots:
-    cls: type
-    bases: FrozenSet[type]
 
 
 @click.command()
@@ -159,16 +61,111 @@ def cli(modulename: str, verbose: bool) -> None:
             if verbose:
                 for cls in slotless_bases:
                     print(f"       - {_class_fullname(cls)}")
-        exit(1)
     else:
+        print("All OK!")
+
+    if verbose:
         print(
-            """\
-All OK!
+            """
 Classes scanned:
-  with slots:              {}
-  without slots:           {}
-""".format(
+  with slots:      {}
+  without slots:   {}
+  non pure Python: {}""".format(
                 len(classes[SlotsStatus.HAS_SLOTS]),
                 len(classes[SlotsStatus.NO_SLOTS]),
-            )
+                len(classes[SlotsStatus.NOT_PUREPYTHON]),
+            ),
+            file=sys.stderr,
         )
+
+    if broken_slots:
+        exit(1)
+
+
+@enum.unique
+class SlotsStatus(enum.Enum):
+    NO_SLOTS = enum.auto()
+    HAS_SLOTS = enum.auto()
+    NOT_PUREPYTHON = enum.auto()
+
+
+def walk_classes(module: ModuleType) -> Iterator[type]:
+    yield from (
+        klass
+        for _, klass in inspect.getmembers(module, inspect.isclass)
+        if klass.__module__.startswith(module.__name__)
+    )
+    try:
+        module_path = module.__path__[0]  # type: ignore
+    except (AttributeError, IndexError):
+        return  # it's not a package with submodules
+
+    packages = pkgutil.walk_packages([module_path])
+    for finder, name, _ in packages:
+        if not getattr(finder, "path", "").startswith(module_path):
+            continue
+
+        module_name = f"{module.__name__}.{name}"
+        try:
+            next_module = importlib.import_module(module_name)
+        except BaseException as e:
+            # sometimes packages make it impossible to import
+            # certain modules directly or out of order, or without
+            # some system or dependency requirement.
+            logger.warning("Couldn't import %s. (%r)", module_name, e)
+        else:
+            yield from walk_classes(next_module)
+
+
+def slot_status(c: type) -> SlotsStatus:
+    if "__slots__" in c.__dict__:
+        return SlotsStatus.HAS_SLOTS
+    elif _is_purepython_class(c):
+        return SlotsStatus.NO_SLOTS
+    else:
+        return SlotsStatus.NOT_PUREPYTHON
+
+
+def _class_fullname(k: type) -> str:
+    return f"{k.__module__}.{k.__qualname__}"
+
+
+def _all_bases_have_slots(c: type) -> bool:
+    return all(slot_status(b) is not SlotsStatus.NO_SLOTS for b in c.__bases__)
+
+
+_UNSETTABLE_ATTRITUBE_MSG = (
+    "cannot set '_SLOTSCHECK_POKE' attribute of immutable type"
+    if sys.version_info > (3, 8)
+    else "can't set attributes of built-in/extension type"
+)
+
+
+def _is_purepython_class(t: type) -> bool:
+    "Whether a class is defined in Python, or an extension/C module"
+    # There is no easy way to check if a class is pure Python.
+    # One symptom of a non-native class is that it is not possible to
+    # set attributes on it. Let's do that.
+    try:
+        t._SLOTSCHECK_POKE = 1  # type: ignore
+    except TypeError as e:
+        if e.args[0].startswith(_UNSETTABLE_ATTRITUBE_MSG):
+            return False
+        raise  # some other error we may want to know about
+    else:
+        del t._SLOTSCHECK_POKE  # type: ignore
+        return True
+
+
+_T1 = TypeVar("_T1")
+_T2 = TypeVar("_T2")
+
+
+def _groupby(
+    it: Iterable[_T1], key: Callable[[_T1], _T2]
+) -> Mapping[_T2, Collection[_T1]]:
+    grouped = defaultdict(list)
+    for i in it:
+        grouped[key(i)].append(i)
+
+    return grouped
