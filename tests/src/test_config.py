561bece7ec930055095325f1c2cd8c8d5b875391
q0w <43147888+q0w@users.noreply.github.com>
2022-02-05 17:21:17 +0300

    Add ini support

--- tests/src/test_config.py
+++ tests/src/test_config.py
@@ -1,3 +1,4 @@
+import dataclasses
 import re
 from pathlib import Path
 
@@ -10,6 +11,8 @@ from slotscheck.config import (
     InvalidKeys,
     InvalidValueType,
     PartialConfig,
+    collect,
+    find_ini_file,
     find_pyproject_toml,
 )
 
@@ -26,6 +29,20 @@ exclude-modules = '''
   |some\\.specific\\.module
 )
 '''
+require-superclass = false
+"""
+
+EXAMPLE_INI = """
+[foo]
+bla = 5
+
+[slotscheck]
+require-subclass = false
+exclude-modules = (
+    .*\\.test\\..*
+    |__main__
+    |some\\.specific\\.module
+    )
 require-superclass = false
 """
 
@@ -58,6 +75,41 @@ def test_find_pyproject_toml(tmpdir):
         == "1"
     )
     assert find_pyproject_toml(Path(tmpdir)) is None
+
+
+def test_find_setup_cfg(tmpdir):
+    (tmpdir.ensure_dir("foo", "bar") / "setup.cfg").write_text(
+        "5", encoding="utf-8"
+    )
+    (tmpdir.ensure_dir("foo") / "setup.cfg").write_text("3", encoding="utf-8")
+    (tmpdir.ensure_dir("foo", "baz") / "setup.cfg").write_text(
+        "1", encoding="utf-8"
+    )
+    tmpdir.ensure_dir("foo", "qux")
+    assert (
+        find_ini_file(Path(tmpdir / "foo/qux")).read_text()  # type: ignore
+        == "3"
+    )
+    assert (
+        find_ini_file(Path(tmpdir / "foo")).read_text() == "3"  # type: ignore
+    )
+    assert (
+        find_ini_file(Path(tmpdir / "foo/bar")).read_text()  # type: ignore
+        == "5"
+    )
+    assert (
+        find_ini_file(Path(tmpdir / "foo/baz")).read_text()  # type: ignore
+        == "1"
+    )
+    assert find_ini_file(Path(tmpdir)) is None
+
+
+def test_collect(tmpdir):
+    (tmpdir / "setup.cfg").write_text(EXAMPLE_INI, encoding="utf-8")
+    (tmpdir / "pyproject.toml").write_binary(EXAMPLE_TOML)
+    assert collect(
+        dataclasses.asdict(Config.EMPTY), Path(tmpdir), None
+    ).require_subclass
 
 
 class TestOptionsApply:
@@ -152,6 +204,86 @@ include-modules = false
             match=re.escape("Invalid value type for 'include-modules'."),
         ):
             PartialConfig.from_toml(Path(tmpdir / "myconf.toml"))
+
+
+class TestPartialOptionsFromIni:
+    def test_options_from_ini(self, tmpdir):
+        (tmpdir / "setup.cfg").write_text(EXAMPLE_INI, encoding="utf-8")
+        Config = PartialConfig.from_ini(Path(tmpdir / "setup.cfg"))
+        assert Config == PartialConfig(
+            strict_imports=None,
+            require_subclass=False,
+            require_superclass=False,
+            include_modules=None,
+            include_classes=None,
+            exclude_classes=None,
+            exclude_modules="""\
+(
+.*\\.test\\..*
+|__main__
+|some\\.specific\\.module
+)""",
+        )
+
+    def test_no_slotscheck_section(self, tmpdir):
+        (tmpdir / "setup.cfg").write_text(
+            """
+[tool.bla]
+k = 5
+""",
+            encoding="utf-8",
+        )
+        assert PartialConfig.from_ini(
+            Path(tmpdir / "setup.cfg")
+        ) == PartialConfig(None, None, None, None, None, None, None)
+
+    def test_empty_slotscheck_section(self, tmpdir):
+        (tmpdir / "myconf.toml").write_text(
+            """
+[slotscheck]
+""",
+            encoding="utf-8",
+        )
+        assert PartialConfig.from_ini(
+            Path(tmpdir / "setup.cfg")
+        ) == PartialConfig(None, None, None, None, None, None, None)
+
+    def test_empty(self, tmpdir):
+        (tmpdir / "setup.cfg").write_text("", encoding="utf-8")
+        assert PartialConfig.from_ini(
+            Path(tmpdir / "setup.cfg")
+        ) == PartialConfig(None, None, None, None, None, None, None)
+
+    def test_invalid_keys(self, tmpdir):
+        (tmpdir / "setup.cfg").write_text(
+            """
+[slotscheck]
+k = 9
+strict-imports = true
+foo = 4
+""",
+            encoding="utf-8",
+        )
+        with pytest.raises(
+            InvalidKeys,
+            match=re.escape("Invalid configuration key(s): 'foo', 'k'."),
+        ):
+            PartialConfig.from_ini(Path(tmpdir / "setup.cfg"))
+
+    def test_invalid_types(self, tmpdir):
+        (tmpdir / "setup.cfg").write_text(
+            """
+[slotscheck]
+strict-imports = true
+include-modules = false
+""",
+            encoding="utf-8",
+        )
+        with pytest.raises(
+            InvalidValueType,
+            match=re.escape("Invalid value type for 'include-modules'."),
+        ):
+            PartialConfig.from_ini(Path(tmpdir / "setup.cfg"))
 
 
 @pytest.mark.parametrize(
